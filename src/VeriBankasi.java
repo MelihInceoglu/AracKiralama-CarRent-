1903feabb09a1c2a72774513c05d8321ffb0bc3d
MelihInceoglu <av.melihinceoglu@gmail.com>
2023-05-04 16:44:12 +0300

    melihnicidegisiklik

--- src/VeriBankasi.java
+++ src/VeriBankasi.java
@@ -49,7 +49,8 @@ public class VeriBankasi {
                 vitesler.add(arb.getVites());
             }
         }
-        System.out.println(vitesler);
+        System.out.println(vitesler); //fsfsfsdfsffgsffsfsfsfsdffsfsfsfs
+
     }
 
 }
